8277f05d9efd91aba3b9b1112c58deb337f9106b
lucas <lucasscpereira14@gmail.com>
2025-10-24 18:17:25 -0400

    remoção de comentário estranho

--- include/seek2.hpp
+++ include/seek2.hpp
@@ -3,10 +3,6 @@
 
 #include "record.hpp" // Necessário para a definição da struct Artigo
 
-/**
- * @brief Imprime os campos de um registro de Artigo de forma formatada.
- * @param artigo O objeto Artigo cujos dados serão exibidos.
- */
 void print_artigo(const Artigo& artigo);
 
 #endif // SEEK2_HPP
