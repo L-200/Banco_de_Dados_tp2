02a336bc8ccf063ecf003a8c66b852d0ddffbbb5
lucas <lucasscpereira14@gmail.com>
2025-10-24 12:35:43 -0400

    add de função que tava faltando

--- include/upload.hpp
+++ include/upload.hpp
@@ -5,13 +5,10 @@
 #include <vector>
 #include "record.hpp" 
 
+//Remove espaços em branco do início e fim da string (modifica in-place)
+void trim(std::string& s);
 
-/**
- * @brief Converte uma linha de texto do formato CSV para a estrutura Artigo
- * @param line A string contendo uma linha do arquivo CSV
- * @param artigo A referência para o objeto Artigo a ser preenchido
- * @return true se o parsing foi bem-sucedido, false caso contrário
- */
+//"limpa" a linha
 bool parse_csv_line(const std::string& line, Artigo& artigo);
 
 #endif // UPLOAD_HPP
